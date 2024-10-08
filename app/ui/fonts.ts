84b90206aa879161ae794131d19e4dfda3bde234
Qntlz <uzyke9517@gmail.com>
2024-10-08 15:28:05 +0800

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Playfair_Display, Inter } from 'next/font/google';
+
+export const playfairDisplay = Playfair_Display({ subsets: ['latin'], weight:['400','700']});
+export const inter = Inter({ subsets: ['latin'], weight:['300'] });
+
+
+
