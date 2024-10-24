c21091155fe54a4ceb3bcc9a999da52652cb2aaa
Qntlz <uzyke9517@gmail.com>
2024-10-24 09:34:10 +0800

    Remove Header Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,14 @@
-"use client";
-
-import { usePathname } from "next/navigation"; // Import the hook
 import "./ui/globals.css";
-import Header from "./ui/home/header/navbar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname(); // Get the current route
-
-  // Define routes where the Header should be hidden
-  const hideHeaderRoutes = ["/login", "/signup", "/"];
 
   return (
     <html lang="en">
       <body>
-        {/* Conditionally render Header only on routes not listed in hideHeaderRoutes */}
-        {!hideHeaderRoutes.includes(pathname) && <Header />}
         {children}
       </body>
     </html>
