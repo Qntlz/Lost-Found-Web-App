5da2630045bee92bab450f4f712f7be8bddb4feb
Qntlz <uzyke9517@gmail.com>
2024-10-25 16:08:53 +0800

    Add useRouter hook

--- app/ui/home/header/signout.tsx
+++ app/ui/home/header/signout.tsx
@@ -1,13 +1,26 @@
 import { signOut } from 'firebase/auth';
-import { auth } from '@/firebaseConfig'; // Replace with your Firebase config path
+import { auth } from '@/firebaseConfig';
+import { useRouter } from 'next/navigation';
 
-export default async function LogoutButton() {
-    try {
-        await signOut(auth); // Log out the user
-        console.log("User signed out successfully.");
-    } catch (error) {
-        console.error("Error signing out:", error);
-    }
+export default function SignOutButton() {
+    const router = useRouter();
 
-};
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth); // Log out the user
+            console.log("User signed out successfully.");
+            router.push('/'); // Redirect to the landing page
+        } catch (error) {
+            console.error("Error signing out:", error);
+        }
+    };
 
+    return (
+        <button
+            className="block w-full px-4 py-2 text-gray-800 hover:bg-gray-100 text-start"
+            onClick={handleSignOut}
+        >
+            Sign Out
+        </button>
+    );
+}
