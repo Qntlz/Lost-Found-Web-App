938dfc22bccd467086b706701be11e7434b4ebc5
Qntlz <uzyke9517@gmail.com>
2024-10-26 08:05:50 +0800

    Update to a a different logo

--- app/ui/home/header/navbar.tsx
+++ app/ui/home/header/navbar.tsx
@@ -13,14 +13,14 @@ export default function Header() {
       <header className="bg-white p-4 flex flex-col md:flex-row justify-between items-center border-b-2">
         {/* Left: Logo */}
         <div className="flex items-center justify-between w-full md:w-auto">
-          <div className="w-10 h-12 rounded-full flex items-center justify-center text-white text-xl">
+          <div className="flex w-10 md:mr-20">
             <Link href="/home">
               <Image
-                src="/logo.svg"
+                src="/logo_text.svg"
                 alt="Logo"
-                width={100}
+                width={40}
                 height={100}
-                className="object-contain transition-transform duration-300 ease-in-out hover:scale-110"
+                className="object-contain scale-[5] translate-x-20"
                 priority
               />
             </Link>
@@ -37,11 +37,11 @@ export default function Header() {
 
         {/* Center: Search Bar */}
         <div className="mt-3 md:mt-0 sm:mx-5 w-full md:w-[680px] flex justify-center order-3 md:order-2">
-          <div className="relative w-full ">
+          <div className="relative w-full md:w-[63%] lg:w-[70%]">
             <input
               type="text"
               placeholder="Search by tags (e.g., red, aquaflask)"
-              className="border border-gray-300 rounded-lg p-2 w-full"
+              className="border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-gray-500"
             />
             <button className="absolute right-0 top-0 h-full px-3 rounded-r-lg">
               <MagnifyingGlassCircleIcon className="h-6 w-6 text-gray-500" />
