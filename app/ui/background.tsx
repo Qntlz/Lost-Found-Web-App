1aa273b1ad9ef09e416f697351dd49865e03dee0
Qntlz <uzyke9517@gmail.com>
2024-10-28 01:44:23 +0800

    Initial Commit

---
+++ app/ui/background.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+
+export default function AnimatedBackground() {
+    return (
+        <>
+            {/* <div className="background-gradient"></div> */}
+            <div className="animated-bg hidden md:block">
+                <div className="icon icon1"></div>
+                <div className="icon icon2"></div>
+                <div className="icon icon3"></div>
+                <div className="icon icon4"></div>
+                <div className="icon icon5"></div>
+                <div className="icon icon6"></div>
+            </div>
+        </> 
+    );
+}
