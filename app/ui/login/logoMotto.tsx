49df9fa09ebb7d3784f0963d8abc09c1b58366d1
Qntlz <uzyke9517@gmail.com>
2024-10-28 08:21:42 +0800

    Improve Responsive Design

--- app/ui/login/logoMotto.tsx
+++ app/ui/login/logoMotto.tsx
@@ -4,7 +4,7 @@ import { playfairDisplay } from "../fonts";
 
 export default function LogoMotto() {
     return (
-        <div className="flex-1 flex flex-col items-center justify-center bg-white text-red-400 p-4 md:p-0">
+        <div className="flex flex-col items-center justify-center bg-white text-red-400 pb-5 md:p-7 lg:p-0 lg:max-[1279px]:pb-5 lg:pb-8 md:max-lg:mt-20 xl:m-24">
             <div className="flex items-center justify-center h-48">
                 <Link href={"/"}>
                     <Image
@@ -19,10 +19,10 @@ export default function LogoMotto() {
                 </Link>
             </div>
             <h1 className={`${playfairDisplay.className} text-4xl font-bold mb-4`}>Lost & Found</h1>
-            <p className="text-lg text-center px-4">
-                "Search and Retrieve your precious items hassle-free"
+            <p className="text-lg text-center px-4 ">
+                Search and Retrieve
+                <span className="block"> your precious items hassle-free</span>
             </p>
         </div>
-
     );
 }
