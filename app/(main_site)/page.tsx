f3f1284c68e6713894b3c1bde0ac813c8b53276e
Qntlz <uzyke9517@gmail.com>
2024-10-29 03:15:11 +0800

    Improve Responsive Design

--- app/(main_site)/page.tsx
+++ app/(main_site)/page.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
 
 export default function LandingPage () {
   return (
-      <div className="min-h-screen flex flex-col ">
+      <div className="flex flex-col min-h-screen">
         {/* Navbar */}
-        <header className="">
-          <nav className="max-w-full mx-auto px-4 mt-2 sm:px-6 lg:px-8 flex justify-between items-center">
+        <header>
+          <nav className="flex max-w-full mx-auto px-4 mt-2 sm:px-6 lg:px-8 justify-between items-center">
             {/* Logo */}
             <div className="flex items-start">
               <Link href={"/"}>
@@ -30,7 +30,7 @@ export default function LandingPage () {
             {/* Login Link */}
             <div>
               <Link href="/login">
-                <p className={`${inter.className} text-red-500 font-medium text-base transition ease-in-out hover:-translate-y-1 hover:scale-110 antialiased`}>
+                <p className={`${inter.className} font-medium text-base text-red-500 transition ease-in-out hover:-translate-y-1 hover:scale-110 antialiased`}>
                   LOG IN
                 </p>
               </Link>
@@ -40,23 +40,23 @@ export default function LandingPage () {
 
         {/* Hero Section */}
         < AnimatedBackground />
-        <section className="flex-1 flex items-center justify-center text-center antialiased">
+        <section className="flex flex-1 text-center items-center justify-center antialiased">
           <div className="max-w-4xl px-6 mb-20 sm:mb-40">
-            <h1 className={`${playfairDisplay.className} text-7xl font-bold text-gray-800 mb-8 pt-16`}>
+            <h1 className={`${playfairDisplay.className} mb-8 pt-16 text-7xl font-bold text-gray-800`}>
               LOST & FOUND
             </h1>
-            <p className={`${inter.className} text-2xl text-red-600 font-semibold`}>
+            <p className={`${inter.className} font-semibold text-2xl text-red-600`}>
               "Find What’s Lost, Faster"
             </p>
             <div className='mt-10 mb-5'>
-              <button className='py-2 rounded-md transition ease-in-out delay-75 outline outline-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 hover:outline-none duration-300'>
+              <button className='py-2 outline outline-red-500 rounded-md transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 hover:outline-none duration-300'>
                 <Link href="/login" className="px-8 py-3 text-red-600 font-bold hover:text-white">
                   Get Started
                 </Link>
               </button>
             </div>
             <div className=''>
-              <p className={`${inter.className} px-8 md:px-28 lg:px-40 py-5 text-center break-words text-lg text-gray-800`}>
+              <p className={`${inter.className} py-5 px-7 md:px-36 lg:px-40 text-lg text-center break-words text-gray-800`}>
                 <span className="text-red-500">Lost & Found</span> is a streamlined platform designed to help users quickly recover lost items.
                 Making the process easy, efficient, and stress-free.
               </p>
@@ -65,10 +65,10 @@ export default function LandingPage () {
         </section>
 
         {/* Features Section */}
-        <section className="bg-white py-24 ">
-          <div className="max-w-7xl mb-32 mx-auto px-4 sm:px-6 lg:px-8 grid gap-12 grid-cols-1 md:grid-cols-3">
+        <section className="bg-white py-24">
+          <div className="grid gap-12 grid-cols-1 max-w-7xl px-4 mb-32 mx-auto sm:px-6  md:grid-cols-3 lg:px-8">
             <div className="text-center">
-              <div className="flex justify-center mb-4 mt-5">
+              <div className="flex mb-4 mt-5 justify-center ">
                 <Image
                   src="/Centralized.svg"
                   alt="Feature 1"
@@ -78,7 +78,7 @@ export default function LandingPage () {
                   className="transition-transform duration-300 ease-in-out hover:scale-110"
                 />
               </div>
-              <h3 className="text-xl font-bold text-gray-800 mb-2">Centralized Platform</h3>
+              <h3 className="mb-2 text-xl font-bold text-gray-800">Centralized Platform</h3>
               <p className="text-gray-600">
                 Manage all your lost and found items in one
                 <span className="block sm:inline">  convenient platform.</span>
@@ -86,7 +86,7 @@ export default function LandingPage () {
             </div>
 
             <div className="text-center">
-              <div className="flex justify-center mb-4 mt-5">
+              <div className="flex mb-4 mt-5 justify-center ">
                 <Image
                   src="/RealTime.svg"
                   alt="Feature 2"
@@ -96,14 +96,14 @@ export default function LandingPage () {
                   className="transition-transform duration-300 ease-in-out hover:scale-110"
                 />
               </div>
-              <h3 className="text-xl font-bold text-gray-800 mb-2">Real-time Updates</h3>
+              <h3 className="mb-2 text-xl font-bold text-gray-800">Real-time Updates</h3>
               <p className="text-gray-600">
                 Stay informed with instant notifications on 
                 <span className="block sm:inline"> found items.</span>
               </p>
             </div>
             <div className="text-center">
-              <div className="flex justify-center mb-4 mt-5">
+              <div className="mb-4 mt-5 flex justify-center">
                 <Image
                   src="/SimpleEfficient.svg"
                   alt="Feature 3"
@@ -113,7 +113,7 @@ export default function LandingPage () {
                   className="transition-transform duration-300 ease-in-out hover:scale-110"
                 />
               </div>
-              <h3 className="text-xl font-bold text-gray-800 mb-2">Simple & Efficient</h3>
+              <h3 className="mb-2 text-xl font-bold text-gray-800">Simple & Efficient</h3>
               <p className="text-gray-600">
                 Easily track and retrieve items with
                 <span className="block sm:inline"> minimal effort.</span>
@@ -123,8 +123,8 @@ export default function LandingPage () {
         </section>
 
         {/* Footer */}
-        <footer className="pb-2 bg-none bg-white">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-red-400">
+        <footer className="bg-white pb-2">
+          <div className="max-w-7xl mx-auto px-4 text-center text-red-400 sm:px-6 lg:px-8">
             &#169; {new Date().getFullYear()} Lost & Found&#8482; . All rights reserved.
           </div>
         </footer>
