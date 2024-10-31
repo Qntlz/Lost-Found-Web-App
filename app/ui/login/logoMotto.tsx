b118659b4693f8f239838c5dde2b73ecca15c5fa
Qntlz <uzyke9517@gmail.com>
2024-10-31 20:47:11 +0800

    Update Component Design

--- app/ui/login/logoMotto.tsx
+++ app/ui/login/logoMotto.tsx
@@ -4,22 +4,21 @@ import { playfairDisplay } from "../fonts";
 
 export default function LogoMotto() {
     return (
-        <div className="flex flex-col items-center justify-center bg-white text-red-400 pb-5 md:p-7 lg:p-0 lg:max-[1279px]:pb-5 lg:pb-8 md:max-lg:mt-20 xl:m-24">
-            <div className="flex items-center justify-center h-48">
+        <div className="flex flex-col items-center justify-center bg-white text-red-500 pb-5 md:p-7 lg:p-0 lg:pb-8 xl:m-24">
+            <div className="flex items-center justify-center">
                 <Link href={"/"}>
                     <Image
                         src="/logo.svg" // Replace with your logo path
                         alt="Logo"
                         width={0}
                         height={0}
-                        style={{ width: 190, height: 'auto' }}
+                        className="w-[150px] transition-transform duration-300 ease-in-out hover:scale-110 portrait-1024:w-[250px]"
                         priority
-                        className="object-contain transition-transform duration-300 ease-in-out hover:scale-110"
                     />
                 </Link>
             </div>
-            <h1 className={`${playfairDisplay.className} text-4xl font-bold mb-4`}>Lost & Found</h1>
-            <p className="text-lg text-center px-4 ">
+            <h1 className={`${playfairDisplay.className} text-4xl font-bold mb-4 portrait-1024:text-5xl xl:text-5xl`}>Lost & Found</h1>
+            <p className="text-lg text-center text-gray-500 px-4 portrait-1024:text-xl">
                 Search and Retrieve
                 <span className="block"> your precious items hassle-free</span>
             </p>
