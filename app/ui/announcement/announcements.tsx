2ad110adef6a3eee5b97e0f8262856b9daf9b576
Qntlz <uzyke9517@gmail.com>
2024-11-04 07:41:33 +0800

    Major style updates

--- app/ui/announcement/announcements.tsx
+++ app/ui/announcement/announcements.tsx
@@ -1,26 +1,33 @@
 import Image from 'next/image';
 import { inter } from '../fonts';
 
-export default function Announcements (){
-    return (
-        <div className="relative">
-            <h2 className={`${inter.className} font-bold`}>Announcements</h2>
-            <div className="mt-3 mx-3">
-              <div className="flex items-center">
-                <Image
-                  src="/logo.svg"
-                  alt="Profile"
-                  width={100}
-                  height={100}
-                  className="h-12 w-12 rounded-full mr-4"
-                />
-                <div>
-                  <h3 className="font-bold text-lg">Carl Omega</h3>
-                  <p className="text-xs text-gray-400">Omega Department Admin</p>
-                </div>
-              </div>
-              <p className="mt-4 text-sm text-justify break-all">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore, dolores voluptatum rerum non officia, saepe at placeat natus, vel eos voluptates commodi unde cupiditate! Obcaecati placeat quod eos natus!</p>
-            </div>
+export default function Announcements () {
+  return (
+    <div className="relative p-2 pr-8 max-w-full overflow-hidden">
+      <h2 className={`${inter.className} font-bold mx-3`}>Announcements</h2>
+      <div className="mt-3 mx-3">
+        <div className="flex items-center">
+          <Image
+            src="/logo.svg"
+            alt="Profile"
+            width={100}
+            height={100}
+            className="h-12 w-12 rounded-full mr-4"
+          />
+          <div>
+            <h3 className="font-bold text-sm">Carl Omega</h3>
+            <p className="text-sm text-gray-400">Omega Department Admin</p>
+          </div>
         </div>
-    );
+        <div className="mt-4 text-xs text-justify break-words">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda labore, dolores voluptatum rerum non officia, saepe 
+            at placeat natus, vel eos voluptates commodi unde cupiditate! 
+            Obcaecati placeat quod eos natus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/ui/home/postFeed.tsx
+++ app/ui/home/postFeed.tsx
@@ -19,25 +19,12 @@ export default function PostFeed() {
 
   return (
     <div className="relative">
-      <div className="bg-white px-4 rounded-lg mb-4">
 
-        {/* Title Header */}
-        <div className={`${paddingClass} flex justify-between`}>
-          <div className="flex">
-            <span className="font-semibold text-2xl text-red-500">{title}</span>
-          </div>
+      <div className="bg-white mx-7 rounded-lg mb-4">
 
-          <div className="flex p-1 rounded-md border border-red-500 justify-center text-red-500">
-            <div className="flex justify-center">
-              <button className="flex font-normal text-sm text-nowrap items-center">
-                < PlusIcon className="mx-1 w-5" />
-                Add Post</button>
-            </div>
-          </div>
-        </div>
-
+        {/* Post Section */}
         <div className="flex flex-col lg:flex-row items-start space-x-0 lg:space-x-4">
-          {/* Post Left Section */}
+          {/* Image */}
           <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
             <Image
               src="/logo.svg"
@@ -48,6 +35,7 @@ export default function PostFeed() {
               className="transition-transform duration-300 ease-in-out hover:scale-110"
             />
           </div>
+          {/* Post Details */}
           <div className="flex-1 mt-4 lg:mt-0">
             <h3 className="font-bold text-lg">Iphone 16 Pro Max</h3>
             <p className="font-medium">Status: <span className={clsx(
@@ -70,8 +58,8 @@ export default function PostFeed() {
           </div>
         </div>
 
-        {/* Reactions */}
-        <div className="mt-4 flex flex-col lg:flex-row justify-between items-start lg:items-center font-light">
+        {/* Post Reactions */}
+        <div className="mt-4 flex font-light md:flex-col lg:flex-row justify-end md:justify-between items-start lg:items-center ">
           <div className="text-gray-500 flex items-center mb-2 lg:mb-0">
             <span className="mr-2">100 views</span>
           </div>
@@ -80,6 +68,203 @@ export default function PostFeed() {
           </div>
         </div>
       </div>
+
+      <div className="bg-white mx-7 rounded-lg mb-4">
+
+        {/* Post Section */}
+        <div className="flex flex-col lg:flex-row items-start space-x-0 lg:space-x-4">
+          {/* Image */}
+          <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={0}
+              height={0}
+              style={{ width: 200, height: 'auto' }}
+              className="transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
+          {/* Post Details */}
+          <div className="flex-1 mt-4 lg:mt-0">
+            <h3 className="font-bold text-lg">Iphone 16 Pro Max</h3>
+            <p className="font-medium">Status: <span className={clsx(
+              {
+                'text-red-500': 'pending' === 'pending',
+                'text-green-500': 'Found' === 'Found',
+              }
+            )}>
+              Found </span>
+            </p>
+            <p className="mt-2 font-medium">Building: LRC</p>
+            <p className="mt-2">Found at the second floor game zone area first table.</p>
+
+            {/* Tags */}
+            <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Red</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">BTS Photocard</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Panda Sticker</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Post Reactions */}
+        <div className="mt-4 flex font-light md:flex-col lg:flex-row justify-end md:justify-between items-start lg:items-center ">
+          <div className="text-gray-500 flex items-center mb-2 lg:mb-0">
+            <span className="mr-2">100 views</span>
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <ArrowTrendingUpIcon className='h-6 w-6 mr-2' /> 100
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white mx-7 rounded-lg mb-4">
+
+        {/* Post Section */}
+        <div className="flex flex-col lg:flex-row items-start space-x-0 lg:space-x-4">
+          {/* Image */}
+          <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={0}
+              height={0}
+              style={{ width: 200, height: 'auto' }}
+              className="transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
+          {/* Post Details */}
+          <div className="flex-1 mt-4 lg:mt-0">
+            <h3 className="font-bold text-lg">Iphone 16 Pro Max</h3>
+            <p className="font-medium">Status: <span className={clsx(
+              {
+                'text-red-500': 'pending' === 'pending',
+                'text-green-500': 'Found' === 'Found',
+              }
+            )}>
+              Found </span>
+            </p>
+            <p className="mt-2 font-medium">Building: LRC</p>
+            <p className="mt-2">Found at the second floor game zone area first table.</p>
+
+            {/* Tags */}
+            <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Red</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">BTS Photocard</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Panda Sticker</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Post Reactions */}
+        <div className="mt-4 flex font-light md:flex-col lg:flex-row justify-end md:justify-between items-start lg:items-center ">
+          <div className="text-gray-500 flex items-center mb-2 lg:mb-0">
+            <span className="mr-2">100 views</span>
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <ArrowTrendingUpIcon className='h-6 w-6 mr-2' /> 100
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white mx-7 rounded-lg mb-4">
+
+        {/* Post Section */}
+        <div className="flex flex-col lg:flex-row items-start space-x-0 lg:space-x-4">
+          {/* Image */}
+          <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={0}
+              height={0}
+              style={{ width: 200, height: 'auto' }}
+              className="transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
+          {/* Post Details */}
+          <div className="flex-1 mt-4 lg:mt-0">
+            <h3 className="font-bold text-lg">Iphone 16 Pro Max</h3>
+            <p className="font-medium">Status: <span className={clsx(
+              {
+                'text-red-500': 'pending' === 'pending',
+                'text-green-500': 'Found' === 'Found',
+              }
+            )}>
+              Found </span>
+            </p>
+            <p className="mt-2 font-medium">Building: LRC</p>
+            <p className="mt-2">Found at the second floor game zone area first table.</p>
+
+            {/* Tags */}
+            <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Red</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">BTS Photocard</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Panda Sticker</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Post Reactions */}
+        <div className="mt-4 flex font-light md:flex-col lg:flex-row justify-end md:justify-between items-start lg:items-center ">
+          <div className="text-gray-500 flex items-center mb-2 lg:mb-0">
+            <span className="mr-2">100 views</span>
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <ArrowTrendingUpIcon className='h-6 w-6 mr-2' /> 100
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white mx-7 rounded-lg mb-4">
+
+        {/* Post Section */}
+        <div className="flex flex-col lg:flex-row items-start space-x-0 lg:space-x-4">
+          {/* Image */}
+          <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={0}
+              height={0}
+              style={{ width: 200, height: 'auto' }}
+              className="transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
+          {/* Post Details */}
+          <div className="flex-1 mt-4 lg:mt-0">
+            <h3 className="font-bold text-lg">Iphone 16 Pro Max</h3>
+            <p className="font-medium">Status: <span className={clsx(
+              {
+                'text-red-500': 'pending' === 'pending',
+                'text-green-500': 'Found' === 'Found',
+              }
+            )}>
+              Found </span>
+            </p>
+            <p className="mt-2 font-medium">Building: LRC</p>
+            <p className="mt-2">Found at the second floor game zone area first table.</p>
+
+            {/* Tags */}
+            <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Red</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">BTS Photocard</span>
+              <span className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm">Panda Sticker</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Post Reactions */}
+        <div className="mt-4 flex font-light md:flex-col lg:flex-row justify-end md:justify-between items-start lg:items-center ">
+          <div className="text-gray-500 flex items-center mb-2 lg:mb-0">
+            <span className="mr-2">100 views</span>
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <ArrowTrendingUpIcon className='h-6 w-6 mr-2' /> 100
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
