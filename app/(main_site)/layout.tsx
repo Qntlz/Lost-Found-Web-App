45551e3fa02359071af2fdf3b55eecc91d55301c
Qntlz <uzyke9517@gmail.com>
2024-11-06 11:34:37 +0800

    Add SideNavXL Component

--- app/(main_site)/layout.tsx
+++ app/(main_site)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation"; // Import the hook
 import Header from "../ui/header/navbar";
+import SideNavXL from "../ui/home/SidenavSimple";
 
 export default function RootLayout({
   children,
@@ -11,13 +12,16 @@ export default function RootLayout({
   const pathname = usePathname(); // Get the current route
 
   // Define routes where the Header should be hidden
-  const hideHeaderRoutes = ["/login", "/signup","/test","/"];
-  
+  const hideHeaderRoutes = ["/login", "/signup", "/test", "/"];
+
   return (
     <div>
-      {/* Conditionally render Header only on routes not listed in hideHeaderRoutes */}
       <link rel="icon" href="/logo.svg" />
+      {/* Conditionally render Header only on routes not listed in hideHeaderRoutes */}
       {!hideHeaderRoutes.includes(pathname) && <Header />}
+      <div className="xl:flex flex-col">
+        {!hideHeaderRoutes.includes(pathname) && < SideNavXL />}
+      </div>
       {children}
     </div>
   );
