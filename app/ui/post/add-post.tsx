db96c38876242ac0bb07c88757cafec3f95e5668
Qntlz <uzyke9517@gmail.com>
2024-11-17 17:06:10 +0800

    Move Submit Button & Implement Post Submission

--- app/ui/post/add-post.tsx
+++ app/ui/post/add-post.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import { inter } from "../fonts";
 import LostItemForm from "./LostItemForm";
-import { Button } from "@headlessui/react";
 import DisplayProfile from "@/app/lib/getProfile";
 import UsernameDisplay from "@/app/lib/getUsername";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
-
 export default function MakePost() {
+
     return (
         <div className={`${inter.className}flex flex-col mt-20 xl:hidden`}>
 
@@ -22,11 +21,6 @@ export default function MakePost() {
                     </div>
                     <span className="text-2xl font-normal text-red-500">Report Lost Item</span>
                 </div>
-
-                {/* Post Button */}
-                <Button className="inline-flex items-center gap-2 rounded-md bg-red-500 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner focus:outline-none data-[hover]:bg-red-600">
-                    POST
-                </Button>
             </div>
 
             {/* Contents */}
