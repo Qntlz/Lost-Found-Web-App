422ca6331eccc3240562e3c068f5dd523c2ad81b
Qntlz <uzyke9517@gmail.com>
2024-11-27 09:21:53 +0800

    Remove Add Item Button & Replace PostFeed component with new Feed Component

--- app/(main_site)/home/page.tsx
+++ app/(main_site)/home/page.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
 import { Metadata } from 'next';
 import { inter } from '@/app/ui/fonts';
-import PostFeed from '@/app/ui/home/postFeed';
 import ProtectedRoute from '@/app/lib/ProtectedRoute';
-import { PlusIcon } from '@heroicons/react/24/outline';
 import Announcements from '@/app/ui/announcement/announcements';
+import Feed from '@/app/ui/home/feed';
 
 export const metadata: Metadata = {
   title: 'Homepage',
@@ -19,16 +17,8 @@ export default function Homepage() {
 
         {/* Post Content */}
         <div className="lg:col-span-4 lg:col-start-2 xl:col-span-7 xl:col-start-3">
-
-          <div className="w-fit p-1 rounded-md border border-red-500 text-red-500 mt-7 mr-7 justify-self-end">
-            <Link href={'/post/make'} className="flex font-normal text-sm text-nowrap items-center">
-              <PlusIcon className="mx-1 w-5" />
-              Add Item
-            </Link>
-
-          </div>
-          <div className="h-[70vh] mt-7 overflow-y-scroll">
-            < PostFeed />
+          <div className="">
+            < Feed />
           </div>
         </div>
 
