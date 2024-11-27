db8230561f35a2745575603518ff9c94cda6f69e
Qntlz <uzyke9517@gmail.com>
2024-11-27 09:25:08 +0800

    Initial Commit

---
+++ app/ui/home/feed.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { inter } from "../fonts";
+import { useState } from "react";
+import FeedHeader  from "./feed-header";
+import Posts from "./feed-content";
+import { user, Post } from "@/app/lib/definitions";
+
+export default function Feed(){
+
+  const [items, setItems] = useState<Post[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState("All");
+  const [selectedCampus, setSelectedCampus] = useState("All");
+  const [selectedBuilding, setSelectedBuilding] = useState("All");
+  const [userList, setUserList] = useState<user[]>([]);
+  const [showFilters, setShowFilters] = useState(false); // State to toggle filter visibility
+
+  return(
+    <div className={`${inter.className}`}>
+      < FeedHeader showFilters={showFilters} selectedStatus={selectedStatus}
+        selectedCampus={selectedCampus} selectedBuilding={selectedBuilding} 
+        setSelectedStatus={setSelectedStatus} setSelectedCampus={setSelectedCampus} 
+        setSelectedBuilding={setSelectedBuilding} setShowFilters={setShowFilters}/>
+
+      <div className="h-[70vh] overflow-y-scroll">
+        < Posts userList={userList} selectedStatus={selectedStatus}
+          selectedCampus={selectedCampus} selectedBuilding={selectedBuilding} 
+          items={items} showFilters={showFilters} setItems={setItems} 
+          setUserList={setUserList} />
+      </div>
+
+    </div>
+  );
+}
