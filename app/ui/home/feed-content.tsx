92bf1dc742cce1d9eab483bf570405b3da17f0b5
Qntlz <uzyke9517@gmail.com>
2024-11-29 11:27:09 +0800

    Fix overlap between filter options and posts

--- app/ui/home/feed-content.tsx
+++ app/ui/home/feed-content.tsx
@@ -66,7 +66,7 @@ export default function Posts({
     }, [selectedStatus, selectedCampus, selectedBuilding]); // Refetch items on filter change
 
     return (
-        <div className={`transition-all duration-300 ${showFilters ? "mt-20" : "mt-4"
+        <div className={`transition-all duration-300 ${showFilters ? "pt-20" : "pt-4"
             }`}>
             {/* Display Filtered and Searched Items */}
             {items.length > 0 ? (
