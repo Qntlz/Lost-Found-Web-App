b2c5971ae0a40235179f93e3d4e26fdbf656a03c
Qntlz <uzyke9517@gmail.com>
2024-12-02 08:55:14 +0800

    Update Breakpoint

--- app/(main_site)/post/edit/page.tsx
+++ app/(main_site)/post/edit/page.tsx
@@ -2,7 +2,7 @@ import MyPost from "@/app/ui/post/retrieve-post";
 
 export default function RetrieveMyPost() {
     return (
-        <div className="mt-20 md:ml-52">
+        <div className="mt-20 lg:ml-52">
             < MyPost />
         </div>
     );
