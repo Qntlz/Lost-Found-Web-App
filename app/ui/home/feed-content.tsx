b53c8cc3a1395f9e42faa32cbb6576c7590030b6
Qntlz <uZyke9517@gmail.com>
2024-12-11 09:20:53 +0800

    Optimize style changes

--- app/ui/home/feed-content.tsx
+++ app/ui/home/feed-content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { userListProps, Post, user } from "@/app/lib/definitions";
 import { db } from "@/firebaseConfig";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
@@ -11,6 +11,16 @@ export default function Posts({
     items, userList, selectedStatus, selectedCampus,
     selectedBuilding, showFilters, setUserList, setItems
 }: userListProps) {
+
+    const [showPreview, setShowPreview] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowPreview(true); // Show the preview immediately
+    };
+
+    const handleMouseLeave = () => {
+        setShowPreview(false); // Hide the preview when the cursor leaves
+    };
 
     // Fetch all user data from Firestore
     const fetchUserUIDs = async () => {
@@ -23,7 +33,6 @@ export default function Posts({
             })) as user[]; // Ensure type matches
 
             setUserList(users); // Now works because types align
-            //console.log("Fetched Users:", users);
         } catch (error) {
             console.error("Error fetching user UIDs:", error);
         }
@@ -31,7 +40,6 @@ export default function Posts({
 
     // Fetch all items from Firestore with applied filters
     const fetchAllItems = async () => {
-        
         try {
             const submissionsRef = collection(db, "allSubmissions");
 
@@ -57,7 +65,6 @@ export default function Posts({
         } catch (error) {
             console.error("Error fetching items:", error);
         }
-
     };
 
     useEffect(() => {
@@ -66,78 +73,92 @@ export default function Posts({
     }, [selectedStatus, selectedCampus, selectedBuilding]); // Refetch items on filter change
 
     return (
-        <div className={`transition-all duration-300 ${showFilters ? "pt-12" : "pt-4"
-            }`}>
+        <div className={`transition-all duration-300 ${showFilters ? "pt-18 xl:pt-24" : "pt-4"}`}>
             {/* Display Filtered and Searched Items */}
             {items.length > 0 ? (
                 items.map((item) => (
-                    <div key={item.id} className="bg-white mx-7 rounded-lg mb-6">
-                        {/* Post Section */}
-                        <div className="flex flex-col lg:flex-row">
-                            {/* Image */}
-                            <div className="w-full lg:w-2/5 h-56 flex items-center justify-center">
+                    // Post Section
+                    <div key={item.id} className="flex flex-col mx-7 rounded-lg mb-6 md:flex-row md:gap-8">
+
+                        {/* Item Image */}
+                        <div className="flex w-full justify-center md:basis-2/5">
+                            {item.imageUrl ? (
+                                <Image
+                                    src={item.imageUrl}
+                                    alt={item.itemName || "Item Image"}
+                                    width={200}
+                                    height={200}
+                                    unoptimized
+                                    style={{ width: "auto", height: "100%" }}
+                                    className="object-contain"
+                                />
+                            ) : (
                                 <Image
                                     src="/logo.svg"
-                                    alt="Logo"
-                                    width={0}
-                                    height={0}
-                                    style={{ width: 200, height: "auto" }}
-                                    className="transition-transform duration-300 ease-in-out hover:scale-110"
+                                    alt={item.itemName || "Item Image"}
+                                    width={200}
+                                    height={200}
+                                    style={{ width: 150, height: "100%" }}
                                 />
+                            )}
+                        </div>
+
+                        {/* Item Details */}
+                        <div className="flex-1 mt-4 lg:mt-0">
+                            <h3 className="font-bold text-lg">{item.itemName}</h3>
+                            <p className="font-medium text-gray-500">
+                                {userList.find((user) => user.id === item.user)?.name || "Unknown User"}
+                            </p>
+                            <p className="font-medium mt-3">
+                                Status:{" "}
+                                <span
+                                    className={clsx({
+                                        "text-red-500 font-normal": item.status === "Missing",
+                                        "text-green-500 font-normal": item.status === "Found",
+                                    })}
+                                >
+                                    {item.status}
+                                </span>
+                            </p>
+                            <p className="mt-2 font-medium">Campus:<span className="font-normal">{item.campus}</span></p>
+                            <p className="mt-2 font-medium">Building:<span className="font-normal">{item.building}</span></p>
+                            <p className="mt-2">{item.description}</p>
+
+                            {/* Tags */}
+                            <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
+                                {item.tags.length > 0 ? (
+                                    item.tags.map((tag, index) => (
+                                        <span
+                                            key={`${tag}-${index}`}
+                                            className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm font-normal"
+                                        >
+                                            {tag}
+                                        </span>
+                                    ))
+                                ) : (
+                                    <p>No tags</p>
+                                )}
                             </div>
-
-                            {/* Post Details */}
-                            <div className="flex-1 mt-4 lg:mt-0">
-                                <h3 className="font-bold text-lg">{item.itemName}</h3>
-                                <p className="font-medium text-gray-500">
-                                    {userList.find((user) => user.id === item.user)?.name || "Unknown User"}
-                                </p>
-                                <p className="font-medium mt-3">
-                                    Status:{" "}
-                                    <span
-                                        className={clsx({
-                                            "text-red-500 font-normal": item.status === "Missing",
-                                            "text-green-500 font-normal": item.status === "Found",
-                                        })}
-                                    >
-                                        {item.status}
-                                    </span>
-                                </p>
-                                <p className="mt-2 font-medium">Campus:<span className="font-normal">{item.campus}</span></p>
-                                <p className="mt-2 font-medium">Building:<span className="font-normal">{item.building}</span></p>
-                                <p className="mt-2">{item.description}</p>
-
-                                {/* Tags */}
-                                <div className="flex flex-wrap space-x-2 mt-4 font-medium gap-y-2">
-                                    {item.tags.length > 0 ? (
-                                        item.tags.map((tag, index) => (
-                                            <span
-                                                key={`${tag}-${index}`}
-                                                className="px-3 py-1 outline outline-2 outline-red-500 rounded-full text-sm font-normal"
-                                            >
-                                                {tag}
-                                            </span>
-                                        ))
-                                    ) : (
-                                        <p>No tags</p>
-                                    )}
-                                </div>
-                                {/* Post Date */}
-                                <div className="text-gray-500 font-light">
-                                    <div className="mt-4 flex justify-end items-start md:flex-row lg:items-center">                               
-                                        <div>
-                                            <span> {item.submittedAt?.toDate().toLocaleDateString()} {item.submittedAt?.toDate().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
-                                        </div>
+                            {/* Post Date */}
+                            <div className="text-gray-500 font-light">
+                                <div className="mt-4 flex justify-end items-start md:flex-row lg:items-center">
+                                    <div>
+                                        <span>
+                                            {item.submittedAt?.toDate().toLocaleDateString()}{" "}
+                                            {item.submittedAt?.toDate().toLocaleTimeString([], {
+                                                hour: "2-digit",
+                                                minute: "2-digit",
+                                            })}
+                                        </span>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 ))
             ) : (
-                <p>No items found.</p>
+                <p className="ml-5">No items found.</p>
             )}
-        </div>
+        </div >
     );
 }
