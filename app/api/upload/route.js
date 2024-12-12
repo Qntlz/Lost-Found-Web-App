9a9803dbedd54eb6f9ad6e3b5199e19981afb13b
Qntlz <uZyke9517@gmail.com>
2024-12-12 13:30:08 +0800

    Import cloudinary config

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,12 +1,5 @@
-import { v2 as cloudinary } from "cloudinary";
+import { cloudinary } from "@/cloudinaryConfig"
 import { NextResponse } from "next/server";
-
-// Configure Cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 export async function POST(req) {
   try {
